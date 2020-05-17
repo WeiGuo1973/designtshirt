e9fea4d7d96eab85263a6669dac07fcb8f12a2bb
WeiGuo1973 <gw_cdm@yahoo.com>
2020-05-17 14:07:09 -0500

    Updates

---
+++ precache-manifest.938f74c1652756b8226afbdf4423f7b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef6188320d471650d87f3d6fd27bdfc",
+    "url": "/designtshirt/index.html"
+  },
+  {
+    "revision": "f2f7a8ef85a0ff40813c",
+    "url": "/designtshirt/static/css/main.489daea2.chunk.css"
+  },
+  {
+    "revision": "c3b154315a08394b04c3",
+    "url": "/designtshirt/static/js/2.00373c26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/designtshirt/static/js/2.00373c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f7a8ef85a0ff40813c",
+    "url": "/designtshirt/static/js/main.0a6e155c.chunk.js"
+  },
+  {
+    "revision": "c3162a0d5d6d9b535c2c",
+    "url": "/designtshirt/static/js/runtime-main.36ea17a0.js"
+  }
+]);
